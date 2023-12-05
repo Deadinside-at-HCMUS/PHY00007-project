cab1879e8d88d070890f36b8f18d6b3bab28bdc6
Tointech <leanhthunk@gmail.com>
2023-12-05 12:51:52 +0700

    [INIT] base frontend

---
+++ frontend/src/Screens/Faculty/Home.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Navbar from "../../components/Navbar";
+import Notice from "../../components/Notice";
+import Profile from "./Profile";
+import Timetable from "./Timetable";
+import { Toaster } from "react-hot-toast";
+import Student from "./Student";
+const Home = () => {
+  const router = useLocation();
+  const navigate = useNavigate();
+  const [selectedMenu, setSelectedMenu] = useState("My Profile");
+  const [load, setLoad] = useState(false);
+  useEffect(() => {
+    if (router.state === null) {
+      navigate("/");
+    }
+    setLoad(true);
+  }, [navigate, router.state]);
+
+  return (
+    <section>
+      {load && (
+        <>
+          <Navbar />
+          <ul className="flex justify-evenly items-center gap-10 w-[85%] mx-auto my-8">
+            <li
+              className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
+                selectedMenu === "My Profile"
+                  ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                  : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
+              }`}
+              onClick={() => setSelectedMenu("My Profile")}
+            >
+              Employee Profile
+            </li>
+            <li
+              className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
+                selectedMenu === "Student Info"
+                  ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                  : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
+              }`}
+              onClick={() => setSelectedMenu("Student Info")}
+            >
+              Student Info
+            </li>
+            <li
+              className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
+                selectedMenu === "Timetable"
+                  ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                  : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
+              }`}
+              onClick={() => setSelectedMenu("Timetable")}
+            >
+              Upload Timetable
+            </li>
+            <li
+              className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
+                selectedMenu === "Notice"
+                  ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                  : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
+              }`}
+              onClick={() => setSelectedMenu("Notice")}
+            >
+              Add Notice
+            </li>
+
+          </ul>
+          <>
+            {selectedMenu === "Timetable" && <Timetable />}
+            {selectedMenu === "Notice" && <Notice />}
+            {selectedMenu === "My Profile" && <Profile />}
+            {selectedMenu === "Student Info" && <Student />}
+          </>
+        </>
+      )}
+      <Toaster position="bottom-center" />
+    </section>
+  );
+};
+
+export default Home;
